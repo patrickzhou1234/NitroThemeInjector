08ac08a00dd1dacee50bf9a3c3061c5849212103
Patrick Zhou <patrickchang44543@gmail.com>
2023-03-16 15:16:14 -0700

    Create UnderTheSea.js

---
+++ UnderTheSea.js
@@ -0,0 +1,18 @@
+fetch("https://discord.com/api/v9/users/@me/settings-proto/1", {
+  "headers": {
+    "authorization": (webpackChunkdiscord_app.push([[""],{},t=>{for(let e in m=[],t.c)m.push(t.c[e])}]),m).find(t=>t?.exports?.default?.getToken!==void 0).exports.default.getToken(),
+    "content-type": "application/json",
+    "sec-ch-ua": "\"Google Chrome\";v=\"111\", \"Not(A:Brand\";v=\"8\", \"Chromium\";v=\"111\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "x-debug-options": "bugReporterEnabled",
+    "x-discord-locale": "en-US",
+    "x-super-properties": ""
+  },
+  "referrer": "https://discord.com/channels/@me/1078470044339679265",
+  "referrerPolicy": "strict-origin-when-cross-origin",
+  "body": "{\"settings\":\"agoIARABGgQSAggP\"}",
+  "method": "PATCH",
+  "mode": "cors",
+  "credentials": "include"
+});
